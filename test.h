38094115a06b412228b89bfd418172475bb77b7d
Steven Draugel <draugelsm@g.cofc.edu>
2017-04-09 10:29:20 -0400

    Create test.h

---
+++ test.h
@@ -0,0 +1,18 @@
+//
+//  Journey.h
+//  VideoPlayer
+//
+//  Created by mac on 6/9/14.
+//  Copyright (c) 2014 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface Journey : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *back;
+
+
+
+@end
