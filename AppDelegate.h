2bc31e634398ad5c3ef9d7c81125a3d206d6d2a7
Steven Draugel <draugelsm@g.cofc.edu>
2017-04-09 10:05:52 -0400

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlackHoleApp
+//
+//  Created by Work on 6/13/14.
+//  Copyright (c) 2014 Winslow DiBona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
